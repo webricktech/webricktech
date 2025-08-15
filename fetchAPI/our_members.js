7ec342f93253ea8956d3d39bc11ceca3c4e32079
Amrit <35948782+CodeByAmrit@users.noreply.github.com>
2025-08-15 19:45:32 +0530

    adding facebook twitter instagram

--- fetchAPI/our_members.js
+++ fetchAPI/our_members.js
@@ -11,7 +11,7 @@ document.addEventListener("DOMContentLoaded", async () => {
 
     container.innerHTML = members
       .map(
-        ({ name, photo, position }) => `
+        ({ name, photo, position, linkedin_url, facebook, twitter, instagram }) => `
         <div class="col-md-6 col-lg-4 mt-2-9 wow fadeInUp" data-wow-delay="200ms">
           <div class="card card-style04 border-radius-10">
             <div class="team-img border">
@@ -23,10 +23,10 @@ document.addEventListener("DOMContentLoaded", async () => {
               </div>
             </div>
             <ul class="mb-0 list-unstyled team-social">
-              <li><a href="#" class="text-white text-primary-hover"><i class="fa-brands fa-facebook-f"></i></a></li>
-              <li><a href="#" class="text-white text-primary-hover"><i class="fa-brands fa-x-twitter"></i></a></li>
-              <li><a href="#" class="text-white text-primary-hover"><i class="fab fa-linkedin-in"></i></a></li>
-              <li><a href="#" class="text-white text-primary-hover"><i class="ti-instagram"></i></a></li>
+              <li><a href="${facebook || ""}" class="text-white text-primary-hover"><i class="fa-brands fa-facebook-f"></i></a></li>
+              <li><a href="${twitter || ""}" class="text-white text-primary-hover"><i class="fa-brands fa-x-twitter"></i></a></li>
+              <li><a href="${linkedin_url || ""}" class="text-white text-primary-hover"><i class="fab fa-linkedin-in"></i></a></li>
+              <li><a href="${instagram || ""}" class="text-white text-primary-hover"><i class="ti-instagram"></i></a></li>
             </ul>
           </div>
         </div>
